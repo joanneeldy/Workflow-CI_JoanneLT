7bab61cd440b224d41ff9ffaf961893dfb1772d3
Joanneeldy <joannelthy@gmail.com>
2025-06-11 20:39:57 +0700

    fixing typo

--- MLProject_folder/modelling.py
+++ MLProject_folder/modelling.py
@@ -29,7 +29,7 @@ mlflow.set_tracking_uri(f"https://dagshub.com/{DAGSHUB_REPO_OWNER}/{DAGSHUB_REPO
 
 # MEMUAT DAN MEMBAGI DATA
 print("Memuat data...")
-df = pd.read_csv('namadataset_preprocessing/mushrooms_preprocessed.csv')
+df = pd.read_csv('mushrooms_preprocessing/mushrooms_preprocessed.csv')
 X = df.drop('class', axis=1)
 y = df['class']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
